210695c8d769103c3c28f943d6c2aac6e4dbef7f
Laura Nunes <lauranunes2508@gmail.com>
2023-01-30 12:05:34 -0300

    insert implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -10,18 +10,18 @@ public class Seller implements Serializable {
 	private Integer id;
 	private String name;
 	private String email;
-	private Date birthdate;
+	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
 	
 	public Seller() {}
 
-	public Seller(Integer id, String name, String email, Date birthdate, Double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.birthdate = birthdate;
+		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
@@ -44,11 +44,11 @@ public class Seller implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;}
 
-	public Date getBirthdate() {
-		return birthdate;}
+	public Date getBirthDate() {
+		return birthDate;}
 
-	public void setBirthdate(Date birthdate) {
-		this.birthdate = birthdate;}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;}
 
 	public Double getBaseSalary() {
 		return baseSalary;}
@@ -81,7 +81,7 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthdate=" + birthdate + ", baseSalary="
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	};
 	
